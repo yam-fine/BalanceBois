5b9f253e097ddda185e0c2a2e1d5d0ce32d90994
yam fine <yamf45@gmail.com>
2021-11-27 21:26:19 +0200

    Scoring system

---
+++ Assets/Scripts/BalanceScale.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalanceScale : MonoBehaviour
+{
+    [SerializeField] Transform leftGoal, rightGoal;
+
+    GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameManager.Instance;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Goal") {
+            if (collision.name == leftGoal.name)
+                gm.LeftScore++;
+            else
+                gm.RightScore++;
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] TMP_Text uiScoreLeft, uiScoreRight;
+
+    int leftScore, rightScore;
+
+    public int LeftScore { get { return leftScore; } set { leftScore = value; uiScoreLeft.text = leftScore.ToString(); } }
+    public int RightScore { get { return rightScore; } set { rightScore = value; uiScoreRight.text = rightScore.ToString(); } }
+
+    static GameManager instance;
+    public static GameManager Instance {
+        get {
+            if (!instance) {
+                instance = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+            }
+            return instance;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            SceneManager.LoadScene("SampleScene");
+    }
+}
