146a68c5c0f3f546b2a261ee6c1b228ac55d815d
yam fine <yamf45@gmail.com>
2021-11-28 01:51:32 +0200

    movement1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 2f;
+
+    private bool jump = false;
+    private float horizontalMove = 0f;
+    CharacterController2D controller;
+
+    private void Start() {
+        controller = GetComponent<CharacterController2D>();
+    }
+
+    void Update()
+    {
+        horizontalMove=Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedTime , false, jump);
+        jump = false;
+    }
+}
